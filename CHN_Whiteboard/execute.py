4fcb470ebb1b3afff0fea7ac54dadebd110bba5f
Himashana <84132436+Himashana@users.noreply.github.com>
2021-06-03 10:02:22 +0530

    Update execute.py

--- CHN_Whiteboard/execute.py
+++ CHN_Whiteboard/execute.py
@@ -1,7 +1,17 @@
 import webbrowser
 f = open("service.js", "a")
+i = open("info.txt", "a")
 x = 0
 y = 0
+print("    |||||||||  |||      |||")
+print("   |||||||     |||      |||")
+print("  ||||||       |||      |||")
+print(" ||            ||||||||||||")
+print("||             ||||||||||||")
+print(" ||            ||||||||||||")
+print("  ||||||       |||      |||")
+print("   |||||||     |||      |||")
+print("    |||||||||  |||      |||")
 while x == 0 :
     open = input("Do you want to start 'service.js' javascript(y/n)? ")
     if open == "y" or open == "Y" :
@@ -31,10 +41,22 @@ while x == 0 :
                 print("Error occured : Undefined")
     elif open == "n" or open == "N":
         print("Exit()")
-        open = input("")
-        f.truncate(0)
-        f.write('location.replace("stoped.html");')
-        f.close()
-        break;
+        exit = input("")
+        if exit == "setup.start --whiteboard" :
+            f.truncate(0)
+            f.write('location.replace("stoped.html");')
+            f.close()
+            print("service.js - Overwrite")
+            i.truncate(0)
+            i.write('CHN_Whiteboard - v1.2.0')
+            i.close()       
+            print("Created info.txt")
+            print("Setup successfully")
+            setup_compleate = input("")
+        else:
+            f.truncate(0)
+            f.write('location.replace("stoped.html");')
+            f.close()
+            break;
     else :
         print("Error occured : Undefined")
