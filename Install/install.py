88e4f6dc1121504913d5184164e1ef61f0f96714
Himashana <84132436+Himashana@users.noreply.github.com>
2021-06-11 13:36:23 +0530

    Add files via upload

---
+++ Install/install.py
@@ -0,0 +1,131 @@
+from tqdm import tqdm
+import time
+print("-------------------------------Setup-------------------------------")
+print("")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100201]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100202]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100203]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100204]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100205]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100206]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100207]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100208]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100209]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100210]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100211]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100212]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100213]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100214]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100215]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100216]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100217]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100218]")
+
+for i in tqdm (range (101),
+			desc="Verifying info: ",
+			ascii=False, ncols=75):
+	time.sleep(0.01)
+	
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100201]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100202]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100203]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100204]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100205]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100206]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100207]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100208]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100209]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100210]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100211]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100212]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100213]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100214]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100215]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100216]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100217]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100218]")
+
+for i in tqdm (range (101),
+			desc="Processing requested data: ",
+			ascii=False, ncols=75):
+	time.sleep(0.01)
+	
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100201]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100202]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100203]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100204]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100205]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100206]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100207]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100208]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100209]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100210]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100211]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100212]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100213]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100214]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100215]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100216]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100217]")
+print("getting information : [https://chnsoftwaredevelopers.com/[...]/files_path_exist/x/n/info.php?request_code=100218]")
+print("")
+print("CHN-Whiteboard-x.x.x/")
+print("├── .github/")
+print("│   ├── workflows/")
+print("│       ├── codeql-analysis.yml")
+print("│       ├── main.yml")
+print("│       ├── npm-publish.yml")
+print("│       ├── python-package.yml")
+print("├── CHN_Whiteboard/")
+print("│   ├── assets/")
+for i in tqdm (range (101),
+			desc="│   │   ├── Screenshot 2021-05-30 151305.png",
+			ascii=False, ncols=75):
+	time.sleep(0.01)
+	
+for i in tqdm (range (210),
+			desc="│   │   ├── assets.txt",
+			ascii=False, ncols=75):
+	time.sleep(0.01)
+	
+for i in tqdm (range (110),
+			desc="│   ├── CSSrefresh.js",
+			ascii=False, ncols=75):
+	time.sleep(0.01)	
+
+for i in tqdm (range (101),
+			desc="│   ├── Update.js",
+			ascii=False, ncols=75):
+	time.sleep(0.01)
+	
+for i in tqdm (range (210),
+			desc="│   ├── Whiteboard.html",
+			ascii=False, ncols=75):
+	time.sleep(0.01)
+	
+for i in tqdm (range (110),
+			desc="│   ├── execute.py",
+			ascii=False, ncols=75):
+	time.sleep(0.01)	
+
+for i in tqdm (range (101),
+			desc="│   ├── run.js",
+			ascii=False, ncols=75):
+	time.sleep(0.01)
+	
+for i in tqdm (range (210),
+			desc="│   ├── service.js",
+			ascii=False, ncols=75):
+	time.sleep(0.01)
+	
+for i in tqdm (range (110),
+			desc="│   ├── stoped.html",
+			ascii=False, ncols=75):
+	time.sleep(0.01)	
+
+print("├── README.md")
+print("├── SECURITY.md")
+print("└── package.json")
+
+
+
