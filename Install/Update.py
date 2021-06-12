fced23095e163964f42291792c27d73475f46744
Himashana <84132436+Himashana@users.noreply.github.com>
2021-06-12 14:39:59 +0530

    Update Update.py

--- Install/Update.py
+++ Install/Update.py
@@ -19,16 +19,37 @@ print("")
 Window1 = Tk()
 Window1.title("CHN Whiteboard Update assistant")
 
+Window2 = Tk()
+Window2.title("CHN Whiteboard Update assistant")
+
 def Update():
     Window1.destroy()
-    print("Updating...")
-    window2 = Tk()
-    print("Running application")
+    #---------------------------Window 2-------------------------
 
-    Label1 = Label(window2, text="CHN Whiteboard Update assistant win 2")
-    Button1 = Button(window2, text="Update now!", command = Update)
-    Label1.pack()
-    Button1.pack()
+windowWidth = Window2.winfo_reqwidth()
+windowHeight = Window2.winfo_reqheight()
+#Window1.geometry("600x400")
+Window2.eval('tk::PlaceWindow . center')
+positionRight = int(Window2.winfo_screenwidth()/2 - windowWidth/2)
+positionDown = int(Window2.winfo_screenheight()/2 - windowHeight/2)
+
+Window2.geometry("+{}+{}".format(positionRight, positionDown))
+
+Label1 = Label(Window2, text="CHN Whiteboard Update assistant")
+Label2 = Label(Window2, text="                                                   ")
+Label3 = Label(Window2, text="       ")
+Label4 = Label(Window2, text="  ")
+Label5 = Label(Window2, text="This setup will guid you to install the latest")
+Label6 = Label(Window2, text="version of CHN Whiteboard.")
+Button1 = Button(Window2, text="Install", command = Update)
+
+Label1.grid(row = 0, column = 0, sticky = W, pady = 2)
+Label2.grid(row = 1, column = 0, sticky = W, pady = 2)
+Label3.grid(row = 1, column = 1, sticky = W, pady = 2)
+Label4.grid(row = 1, column = 2, sticky = W, pady = 2)
+Label5.grid(row = 2, column = 0, sticky = W, pady = 2)
+Label6.grid(row = 3, column = 0, sticky = W, pady = 2)
+Button1.grid(row = 4, column = 1, sticky = W, pady = 2)
 
 
 #---------------------------Window 1-------------------------
@@ -46,16 +67,17 @@ Label1 = Label(Window1, text="CHN Whiteboard Update assistant")
 Label2 = Label(Window1, text="                                                   ")
 Label3 = Label(Window1, text="       ")
 Label4 = Label(Window1, text="  ")
-text= Text(Window1,wrap=WORD)
-text.insert(INSERT,"This setup will guid you to install the latest version of CHN Whiteboard.")
+Label5 = Label(Window1, text="This setup will guid you to install the latest")
+Label6 = Label(Window1, text="version of CHN Whiteboard.")
 Button1 = Button(Window1, text="Next>", command = Update)
 
 Label1.grid(row = 0, column = 0, sticky = W, pady = 2)
 Label2.grid(row = 1, column = 0, sticky = W, pady = 2)
 Label3.grid(row = 1, column = 1, sticky = W, pady = 2)
 Label4.grid(row = 1, column = 2, sticky = W, pady = 2)
-text.grid(row = 2, column = 0, sticky = W, pady = 2)
-Button1.grid(row = 3, column = 1, sticky = W, pady = 2)
+Label5.grid(row = 2, column = 0, sticky = W, pady = 2)
+Label6.grid(row = 3, column = 0, sticky = W, pady = 2)
+Button1.grid(row = 4, column = 1, sticky = W, pady = 2)
 
 
 
